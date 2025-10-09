35c243ed2147f1e72ea364b9acca9cbbecb591fd
Vishal Sikchi <quenchdev4@quenchchargers.com>
2025-10-09 15:34:01 +0530

    update the MSIL HMI

--- hmi/msil/ador-display-splitScreenHMI/src/components/DashBoard/Dashboard.js
+++ hmi/msil/ador-display-splitScreenHMI/src/components/DashBoard/Dashboard.js
@@ -126,7 +126,7 @@ function Dashboard() {
       if (config && config.API) {
         try {
           // Use the API endpoint from config, or fallback to a default endpoint
-          const apiEndpoint = 'http://10.20.27.50:3001/store/charger-config';
+          const apiEndpoint = `${config.API}/store/charger-config`;
           await startupApiCall(config, apiEndpoint);
         } catch (error) {
           // Error is already logged in the startupApiCall function
